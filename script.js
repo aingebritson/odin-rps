6fb363aee347d7128dc5e86bc4bbeb902ceb3a4d
aingebritson <aingebritson@gmail.com>
2025-03-04 20:58:07 -0500

    Write the function to play one round
    Create playRound function
    Define parameters
    Make humanChoice parameter case-insensitive
    Log winner
    Increment human score

--- script.js
+++ script.js
@@ -10,18 +10,53 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
-
 // Get human's choice
 function getHumanChoice() {
     let humanChoice = prompt("Enter 'Rock', 'Paper', or 'Scissors'");
     return humanChoice;
 }
 
-console.log(getHumanChoice());
-
 // Declare players score variables
 let humanScore = 0;
 let computerScore = 0;
 
 // Play single round
+function playRound(humanChoice, computerChoice) {
+    let humanCompare = humanChoice.toLowerCase();
+    let evaluate = humanCompare + computerChoice;
+    switch (evaluate) {
+        case "rockRock":
+        case "paperPaper":
+        case "scissorsScissors":
+            console.log("Tie! Go again!");
+            break;
+        case "rockScissors":
+            console.log("You win! Rock beats Scissors!");
+            humanScore++;
+            break;
+        case "rockPaper":
+            console.log("You lose! Paper beats Rock!");
+            computerScore++;
+            break;
+        case "paperRock":
+            console.log("You win! Paper beats Rock!");
+            humanScore++;
+            break;
+        case "paperScissors":
+            console.log("You lose! Scissors beats Paper!");
+            computerScore++;
+            break;
+        case "scissorsRock":
+            console.log("You lose! Rock beats Scissors!");
+            computerScore++;
+            break;
+        case "scissorsPaper":
+            console.log("You win! Scissors beats Paper!");
+            humanScore++;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
