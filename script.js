58755f2290072197fe90bc238a7cff347f88bd93
aingebritson <aingebritson@gmail.com>
2025-03-04 21:38:13 -0500

    Create gameplay loop

--- script.js
+++ script.js
@@ -16,47 +16,67 @@ function getHumanChoice() {
     return humanChoice;
 }
 
-// Declare players score variables
-let humanScore = 0;
-let computerScore = 0;
+//Play 5 rounds
+function playGame() {
+    // Declare players score variables
+    let humanScore = 0;
+    let computerScore = 0;
 
-// Play single round
-function playRound(humanChoice, computerChoice) {
-    let humanCompare = humanChoice.toLowerCase();
-    let evaluate = humanCompare + computerChoice;
-    switch (evaluate) {
-        case "rockRock":
-        case "paperPaper":
-        case "scissorsScissors":
-            console.log("Tie! Go again!");
-            break;
-        case "rockScissors":
-            console.log("You win! Rock beats Scissors!");
-            humanScore++;
-            break;
-        case "rockPaper":
-            console.log("You lose! Paper beats Rock!");
-            computerScore++;
-            break;
-        case "paperRock":
-            console.log("You win! Paper beats Rock!");
-            humanScore++;
-            break;
-        case "paperScissors":
-            console.log("You lose! Scissors beats Paper!");
-            computerScore++;
-            break;
-        case "scissorsRock":
-            console.log("You lose! Rock beats Scissors!");
-            computerScore++;
-            break;
-        case "scissorsPaper":
-            console.log("You win! Scissors beats Paper!");
-            humanScore++;
+    // Play single round
+    function playRound(humanChoice, computerChoice) {
+        let humanCompare = humanChoice.toLowerCase();
+        let evaluate = humanCompare + computerChoice;
+        switch (evaluate) {
+            case "rockRock":
+            case "paperPaper":
+            case "scissorsScissors":
+                console.log("Tie! Go again!");
+                break;
+            case "rockScissors":
+                console.log("You win! Rock beats Scissors!");
+                humanScore++;
+                break;
+            case "rockPaper":
+                console.log("You lose! Paper beats Rock!");
+                computerScore++;
+                break;
+            case "paperRock":
+                console.log("You win! Paper beats Rock!");
+                humanScore++;
+                break;
+            case "paperScissors":
+                console.log("You lose! Scissors beats Paper!");
+                computerScore++;
+                break;
+            case "scissorsRock":
+                console.log("You lose! Rock beats Scissors!");
+                computerScore++;
+                break;
+            case "scissorsPaper":
+                console.log("You win! Scissors beats Paper!");
+                humanScore++;
+        }
+    }
+
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+
+    playRound(humanSelection, computerSelection);
+
+    console.log("Score: " + humanScore + " to " +computerScore);
+
+    while (humanScore < 5 && computerScore < 5) {
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();        
+        playRound(humanSelection, computerSelection);
+        console.log("Score: " + humanScore + " to " +computerScore);
+    }
+
+    if (computerScore === 5) {
+        console.log ("Game over! You lose!");
+    } else {
+        console.log("Game over! You win!");
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();
